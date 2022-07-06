32f504f7dd6edcf20bc040cfd0ca8901fe021d36
NotInMoodTobi <tobias.airich@gmail.com>
2022-07-06 13:03:09 +0200

    .

---
+++ .next/static/1lptMPdnqMK3IECIhpkPC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/942-5873c870527cbeaf.js","static/chunks/pages/index-64afa84bc5d4a9af.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
